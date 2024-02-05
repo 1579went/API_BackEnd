15c2a1c2fc92cbcf5735cecb9f9df96b5aab37a3
BlackWent <2050446216@qq.com>
2024-02-05 13:32:32 +0800

    修改生产环境 add Dockerfile

--- API_Interface/src/main/java/com/pignest/api_interface/ApiInterfaceApplication.java
+++ API_Interface/src/main/java/com/pignest/api_interface/ApiInterfaceApplication.java
@@ -1,5 +1,6 @@
 package com.pignest.api_interface;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Black
  */
 @SpringBootApplication(scanBasePackages = {"com.pignest"})
+@MapperScan("com.pignest.api_interface.mapper")
 public class ApiInterfaceApplication {
 
     public static void main(String[] args) {
